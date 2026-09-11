a154affbb2d52ab916058e547dcc8763c0bcda69
matejkalc <matejkalc@localhost>
2026-09-11 08:33:10 +0200

    Prepare authorized private update 10 (#48)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "org.alignertracker.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 9
+        versionCode = 10
         versionName = "1.0.0-dev"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
