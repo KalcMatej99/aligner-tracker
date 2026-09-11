9caa8022c59aeda951e1c59bb05d8d78a21f2a9a
matejkalc <matejkalc@localhost>
2026-09-11 10:22:22 +0200

    Prepare private phone update 11 (#53)

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "org.alignertracker.app"
         minSdk = 30
         targetSdk = 36
-        versionCode = 10
+        versionCode = 11
         versionName = "1.0.0-dev"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
